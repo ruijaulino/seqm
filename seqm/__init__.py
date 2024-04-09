72b7cecc80ee894d7b37686b148157522c10fe5d
Rui Jaulino <rui_jaulino@protonmail.com>
2024-04-09 10:49:11 +0100

    clean files

--- seqm/__init__.py
+++ seqm/__init__.py
@@ -1,6 +1,6 @@
 from . import models
-from .data import Element, Elements, Dataset, Path
-from .bt import cvbt
+from .dataset import Dataset
+from .model_pipe import ModelPipe,ModelPipes,Path
 from .transform import BaseTransform, IdleTransform, MeanScaleTransform, ScaleTransform, RollPWScaleTransform
 from .post_process import post_process, portfolio_post_process
 from .models import *
