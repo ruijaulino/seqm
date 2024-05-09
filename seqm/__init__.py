1b6920bad96aca8d56a0225f33f01680f57deac9
Rui Jaulino <rjaulino@protonmail.com>
2024-05-09 18:14:37 +0100

    fix bug in init

--- seqm/__init__.py
+++ seqm/__init__.py
@@ -1,6 +1,6 @@
 from . import models
 from .dataset import Dataset
 from .model_pipe import ModelPipe,ModelPipes,Path
-from .transform import BaseTransform, IdleTransform, MeanScaleTransform, ScaleTransform, RollPWScaleTransform
+from .transform import BaseTransform, IdleTransform, MeanScaleTransform, ScaleTransform, RollPWScaleTransform, InvVolPwTransform
 from .post_process import post_process, portfolio_post_process
 from .models import *
